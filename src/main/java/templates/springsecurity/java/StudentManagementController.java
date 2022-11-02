929577e188dbd4dd9584f2906e0c2370d798479c
Jay Rathod <jay.rathod@simformsolutions.com>
2022-11-02 19:28:20 +0530

    1:52:59 Amigos preautherize ongoing

--- src/main/java/templates/springsecurity/java/StudentManagementController.java
+++ src/main/java/templates/springsecurity/java/StudentManagementController.java
@@ -3,6 +3,7 @@ package templates.springsecurity.java;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,17 +21,20 @@ import lombok.extern.slf4j.Slf4j;
 public class StudentManagementController {
     private static List<Student> students= Arrays.asList(new Student(1,"Jay Rathod"), new Student(2, "Jay Billionaire"), new Student(3, "Jay Millionaire"));
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN, ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents(){
         return students;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(Student student){
       log.info("Student added " + student +"to the list"); 
 
     }
 
     @DeleteMapping(path="{id}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("id") Integer id){
         // Student studDelete = new Student();
         log.info("Student deleted to verify autherization");
@@ -38,6 +42,7 @@ public class StudentManagementController {
 
     }
     @PutMapping(path="{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent( @PathVariable("studentId") Integer id, @RequestBody Student student){
         log.info("Student modified");
     }
